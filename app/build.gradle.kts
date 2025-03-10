38d8763c0959c2cfd64a04b56327ec811251bf6a
Spring Wong <rudolphwong2002@gmail.com>
2025-03-10 02:23:11 -0400

    move deps to correct build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,28 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // lynx dependencies
+    implementation(libs.lynx)
+    implementation(libs.lynx.jssdk)
+    implementation(libs.lynx.trace)
+    implementation(libs.primjs)
+
+    // integrating image-service
+    implementation(libs.lynx.service.image)
+
+    // image-service dependencies, if not added, images cannot be loaded; if the host APP needs to use other image libraries, you can customize the image-service and remove this dependency
+    implementation(libs.fresco)
+    implementation(libs.fresco.animated.gif)
+    implementation(libs.fresco.animated.webp)
+    implementation(libs.fresco.webpsupport)
+    implementation(libs.fresco.animated.base)
+
+    // integrating log-service
+    implementation(libs.lynx.service.log)
+
+    // integrating http-service
+    implementation(libs.lynx.service.http)
+
+    implementation(libs.okhttp)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply true
+    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-}
-
-dependencies {
-    // lynx dependencies
-    implementation(libs.lynx)
-    implementation(libs.lynx.jssdk)
-    implementation(libs.lynx.trace)
-    implementation(libs.primjs)
-
-    // integrating image-service
-    implementation(libs.lynx.service.image)
-
-    // image-service dependencies, if not added, images cannot be loaded; if the host APP needs to use other image libraries, you can customize the image-service and remove this dependency
-    implementation(libs.fresco)
-    implementation(libs.fresco.animated.gif)
-    implementation(libs.fresco.animated.webp)
-    implementation(libs.fresco.webpsupport)
-    implementation(libs.fresco.animated.base)
-
-    // integrating log-service
-    implementation(libs.lynx.service.log)
-
-    // integrating http-service
-    implementation(libs.lynx.service.http)
-
-    implementation(libs.okhttp)
 }
