c251d2d067d6c5b6402aab6a0df542c50542f7c0
Spring Wong <rudolphwong2002@gmail.com>
2025-03-11 00:30:39 -0400

    add padding for clickable in pixel 9

--- app/src/main/java/com/lynx/lynxandroidproject/MainActivity.kt
+++ app/src/main/java/com/lynx/lynxandroidproject/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -54,11 +55,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun TestButton(
     text: String = "Click Me",
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     Button(
         onClick = onClick,
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier.padding(48.dp),
     ) {
         Text(text = text)
     }
